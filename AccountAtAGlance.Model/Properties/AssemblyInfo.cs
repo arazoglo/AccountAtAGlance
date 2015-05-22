b85d4d9aa03ae352b2dbe06beb7457fa0d6901ac
arazoglo <arazoglo@hotmail.com>
2015-05-22 17:13:32 +0100

    Adding Model and Repository projects, updating JS and JS UI libraries, adding project references

--- AccountAtAGlance.Model/Properties/AssemblyInfo.cs
+++ AccountAtAGlance.Model/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AccountAtAGlance")]
+[assembly: AssemblyTitle("AccountAtAGlance.Model")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Open GI Ltd.")]
-[assembly: AssemblyProduct("AccountAtAGlance")]
+[assembly: AssemblyProduct("AccountAtAGlance.Model")]
 [assembly: AssemblyCopyright("Copyright © Open GI Ltd. 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("737d12a5-4321-4e87-8f2c-a73bb4de0a9f")]
+[assembly: Guid("05b28b21-252c-442f-ae07-3a4e2b931fb3")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
